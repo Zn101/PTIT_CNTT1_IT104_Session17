aa0d67a626835ddfc60cd78fb0e367a662d1de16
Nguyen Thac Hung <115692325+Zn101@users.noreply.github.com>
2025-08-26 23:40:38 +0700

    Update Form.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 
 export default function Form() {
-  let [title, setTitle] = useState<string>("");
-  const handleChange = () => {
-    setTitle(title)
-  }
+  const [title, setTitle] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
 
   return (
     <div>
-      <input type="text" placeholder='Nhap noi dung' onChange={handleChange}/>
+      <input 
+        type="text" 
+        placeholder="Nhap noi dung" 
+        value={title}
+        onChange={handleChange} 
+      />
       <p>{title}</p>
     </div>
   );
