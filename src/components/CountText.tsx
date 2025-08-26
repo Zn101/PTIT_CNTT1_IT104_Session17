490a1478757bd0249265ee18e1028f5036c16c0f
Nguyen Thac Hung <115692325+Zn101@users.noreply.github.com>
2025-08-26 23:40:18 +0700

    Update CountText.tsx

--- src/components/CountText.tsx
+++ src/components/CountText.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 
 export default function CountText() {
-  let [count, setCount] = useState<number>(0);
-  const handleChange = () => {
-    setCount(count + 1)
-  }
+  const [count, setCount] = useState<number>(0);
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setCount(e.target.value.length);
+  };
 
   return (
     <div>
-      <textarea placeholder='Nhap noi dung' onChange={handleChange}/>
+      <textarea 
+        placeholder='Nhap noi dung' 
+        onChange={handleChange}
+      />
       <p>So ky tu: {count}</p>
     </div>
   );
